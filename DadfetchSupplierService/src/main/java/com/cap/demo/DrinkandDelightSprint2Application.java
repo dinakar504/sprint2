584ee8540a39a2b817de194450e591c0cb6c9bdb
dinak <dinak@192.168.0.6>
2020-04-28 19:27:59 +0530

    addded supplier

---
+++ DadfetchSupplierService/src/main/java/com/cap/demo/DrinkandDelightSprint2Application.java
@@ -0,0 +1,14 @@
+package com.cap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkandDelightSprint2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkandDelightSprint2Application.class, args);
+	}
+
+
+}
---
+++ DadfetchSupplierService/src/main/java/com/cap/demo/service/ServiceI.java
@@ -0,0 +1,11 @@
+package com.cap.demo.service;
+
+import java.util.List;
+
+import com.cap.demo.Entities.SupplierEntity;
+
+public interface ServiceI {
+
+	List<SupplierEntity> getsup();
+
+}
