e51556635143af359df191a401ce57b321c851f7
dinak <dinakar504>
2020-05-07 20:20:04 +0530

    added Product Order

---
+++ DadCreateProductOrderService2/src/test/java/com/cap/demo/DrinkandDelightSprint2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkandDelightSprint2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
